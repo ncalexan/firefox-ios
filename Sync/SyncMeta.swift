3d589255321b470e23b7fb356f2fefd98d74af15
Richard Newman <rnewman@twinql.com>
2015-03-20 19:40:18 -0700

    Begin fleshing out SyncMeta.

--- Sync/SyncMeta.swift
+++ Sync/SyncMeta.swift
@@ -3,6 +3,41 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import Foundation
+import Shared
+
+public struct EngineMeta {
+    let version: Int
+    let syncID: String
+    public static func mapFromJSON(map: [String: JSON]?) -> [String: EngineMeta]? {
+        if map == nil {
+            return nil
+        }
+        // TODO
+        return [String: EngineMeta]()
+    }
+}
+
+public struct Global {
+    let syncID: String
+    let storageVersion: Int
+    let engines: [String: EngineMeta]?      // Is this really optional?
+    let declined: [String]?
+
+    // TODO: is it more useful to support partial globals?
+    public static func fromJSON(json: JSON) -> Global? {
+        if let syncID = json["syncID"].asString {
+            if let storageVersion = json["storageVersion"].asInt {
+                let engines = EngineMeta.mapFromJSON(json["engines"].asDictionary)
+                let declined = json["declined"].asArray
+                return Global(syncID: syncID,
+                              storageVersion: storageVersion,
+                              engines: engines,
+                              declined: jsonsToStrings(declined))
+            }
+        }
+        return nil
+    }
+}
 
 /**
  * Encapsulates a meta/global, identity-derived keys, and keys.
@@ -11,7 +46,7 @@ public class SyncMeta {
     let syncKey: KeyBundle
 
     var keys: Keys?
-    var global: AnyObject?
+    var global: Global?
 
     public init(syncKey: KeyBundle) {
         self.syncKey = syncKey
