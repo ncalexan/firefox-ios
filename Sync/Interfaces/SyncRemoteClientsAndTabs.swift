2358284507ca20c18524f536f1eefc3d81b51efa
Richard Newman <rnewman@twinql.com>
2015-04-08 12:11:55 -0700

    Future: Add SyncRemoteClientsAndTabs to wrap a DB.

---
+++ Sync/Interfaces/SyncRemoteClientsAndTabs.swift
@@ -0,0 +1,43 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Account
+import Shared
+import Storage
+
+
+public class SyncRemoteClientsAndTabs: RemoteClientsAndTabs {
+    private let db: SQLiteRemoteClientsAndTabs
+    private let account: FirefoxAccount?
+
+    public init(files: FileAccessor, account: FirefoxAccount?) {
+        self.db = SQLiteRemoteClientsAndTabs(files: files)
+        self.account = account
+    }
+
+    public func wipeClients() -> Deferred<Result<()>> {
+        return self.db.wipeClients()
+    }
+
+    public func wipeTabs() -> Deferred<Result<()>> {
+        return self.db.wipeTabs()
+    }
+
+    public func getClients() -> Deferred<Result<[RemoteClient]>> {
+        return self.db.getClients()
+    }
+
+    public func getClientsAndTabs() -> Deferred<Result<[ClientAndTabs]>> {
+        return self.db.getClientsAndTabs()
+    }
+
+    public func insertOrUpdateClient(client: RemoteClient) -> Deferred<Result<()>> {
+        return self.db.insertOrUpdateClient(client)
+    }
+
+    public func insertOrUpdateTabsForClient(client: String, tabs: [RemoteTab]) -> Deferred<Result<Int>> {
+        return self.db.insertOrUpdateTabsForClient(client, tabs: tabs)
+    }
+}
